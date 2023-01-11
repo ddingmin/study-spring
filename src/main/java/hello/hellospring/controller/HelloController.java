b177d2ea4f67c339a8164d2c70b56419e3b4ee3b
ddingmin <ddingminyt@gmail.com>
2023-01-11 14:00:12 +0900

    (1.10)-section2: 스프링 웹 개발 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,10 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.swing.*;
 
 @Controller
 public class HelloController {
@@ -11,4 +15,40 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        // 파라미터를 받을 때는 @RequestParam
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // ResponseBody이 응답 Body부에 return 값을 직접 넣어주겠다는 의미
+    // 데이터가 그대로 전달된다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    // Json 방식으로 반환된다. key와 value의 형태
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
