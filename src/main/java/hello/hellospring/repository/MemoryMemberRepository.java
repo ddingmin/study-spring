85a8d7a59e0d4d76429cb5b6e9a97c332848af81
ddingmin <ddingminyt@gmail.com>
2023-01-11 22:29:04 +0900

    (1.11)-section3(회원 관리 예제): 리포지토리 테스트 구현

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -32,4 +32,8 @@ public class MemoryMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values()); // store에 있는 멤버들 모두 반환
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
---
+++ src/test/java/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,62 @@
+package repository;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach // 메소드가 끝날 때 마다 실행되는 메소드 -> 테스트가 끝날 때 마다 비워준다.
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("ddingmin");
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        // Assertions.assertEquals(member, result); // 기대값, 결과값 (member가 나와야함)
+        assertThat(member).isEqualTo(result); // member가 result와 똑같은지 확인
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("ddingmin1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("ddingmin2");
+        repository.save(member2);
+
+        Member result = repository.findByName("ddingmin1").get();
+        assertThat(result).isEqualTo(member1); // result와 member1이 같은지 확인
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("ddingmin1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("ddingmin2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
