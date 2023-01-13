140597fecf3634618453a835b984b65fbddddedd
ddingmin <ddingminyt@gmail.com>
2023-01-13 21:01:22 +0900

    (1.12)-section3(회원 관리 예제): 멤버 서비스 구현 및 테스트 구현

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,50 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        // 외부에서 넣어주도록 변경
+        this.memberRepository = memberRepository;
+    }
+    // 서비스를 만드려면 멤버 저장소가 필요함.
+
+    // TODO: 회원 가입 로직
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+
+    private void validateDuplicateMember(Member member) { // 같은 이름을 가진 중복 회원이 있으면 안되는 로직
+        //        Optional<Member> result = memberRepository.findByName(member.getName());
+//
+//        // ifPresent란 값이 있다면 동작함.
+//        result.ifPresent(m -> {
+//            // 값이 없다면 예외처리
+//            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+//        });
+        // 위 코드를 줄인 것.
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    // 전체회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,66 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+        // 멤버 서비스는 멤버 리포지토리를 통해 생성 -> 이를 DI (Dependency Injection) 이라고 한다.
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        // memberService.join(member2)를 실행할 때 StateException으로 예외 처리되는지 확인
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        // 위 에외 처리 메시지가 올바르게 출력되는지 확인
+        //then
+    }
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
